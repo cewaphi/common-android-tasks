8d7aa96cf86fcb472c809c7406c8ad867da59d3c
Cedric Wang <cewaphi@outlook.com>
2021-02-02 14:14:39 +0100

    Prepared simple setup with buttons.

--- DataSharing/app/src/main/java/com/example/datasharing/ui/export/SlideshowViewModel.kt
+++ DataSharing/app/src/main/java/com/example/datasharing/ui/export/SlideshowViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.datasharing.ui.slideshow
+package com.example.datasharing.ui.export
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Export"
     }
     val text: LiveData<String> = _text
 }
--- DataSharing/app/src/main/java/com/example/datasharing/ui/import/GalleryViewModel.kt
+++ DataSharing/app/src/main/java/com/example/datasharing/ui/import/GalleryViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.datasharing.ui.gallery
+package com.example.datasharing.ui.import
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Import"
     }
     val text: LiveData<String> = _text
 }
