574277ca6fded741b7afcf4c277d37f12845f73e
Cedric Wang <cewaphi@outlook.com>
2021-02-17 14:30:06 +0100

    Simple use case for detecting one or multiple possible languages.

--- MLKitLanguageDetection/app/src/main/java/com/example/mlkitlanguagedetection/ui/home/HomeFragment.kt
+++ MLKitLanguageDetection/app/src/main/java/com/example/mlkitlanguagedetection/ui/home/HomeFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mlkitlanguagedetection.R
 import com.example.mlkitlanguagedetection.databinding.FragmentHomeBinding
+import com.google.mlkit.nl.languageid.LanguageIdentification
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +22,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val languageIdentifier get() = LanguageIdentification.getClient()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -36,6 +41,36 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDetect.setOnClickListener {
+            try {
+                languageIdentifier.identifyLanguage(binding.inputText.text.toString())
+                    .addOnSuccessListener {
+                        if (it.contains("und", ignoreCase = true)) {
+                            Toast.makeText(requireContext(), "What language is this?!", Toast.LENGTH_SHORT).show()
+                            binding.detectedLanguage.text = "UNKNOWN"
+                        } else {
+                            Toast.makeText(requireContext(), "Success!", Toast.LENGTH_SHORT).show()
+                            binding.detectedLanguage.text = Locale(it).displayName
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(requireContext(), "Failure! :(", Toast.LENGTH_SHORT).show()
+                    }
+                languageIdentifier.identifyPossibleLanguages(binding.inputText.text.toString())
+                    .addOnSuccessListener { identifiedLanguages ->
+                        binding.detectedLanguages.text = identifiedLanguages.map {
+                            "${it.languageTag} (${"%.3f".format(it.confidence)})"
+                        }.joinToString("\n")
+                    }
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Error: $e", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
